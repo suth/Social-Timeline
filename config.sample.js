5e48f000a6d27a8fcf9310ee0544826b4548fe16
Simon Murtha-Smith <simon@murtha-smith.com>
2011-11-18 16:55:39 -0800

    a few tweaks to make the config.sample.js clearer

--- config.sample.js
+++ config.sample.js
@@ -9,7 +9,8 @@ var installation_url = 'http://think.up/directory/'; // URL of your ThinkUp inst
 var username = 'username'; // Username for the Twitter account to display
 var loadPage = thinkup_loadPage; // leave this as is
 
-// Locker/Singly settings, choose an installation URL based on
+// Locker/Singly settings
 var loadPage = locker_loadPage; // leave this as is
+// choose an Locker/Singly installation URL (comment out the other)
 var installation_url = 'http://localhost:8042/'; // For Locker
 var installation_url = 'https://api.singly.com/<your-singly-api-key>/'; // For Singly, don't expose your Singly API key to the public!!!
